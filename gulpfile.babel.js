c5f1996daecc837ee707e6b67ccc316fd8203b1d
Hannes <50320115+hienous@users.noreply.github.com>
2020-09-30 07:11:46 +0300

    added templates for monitori and major refactoring of project (#2)
    * added templates for monitori and major refactoring of project
    
    * updated img alt texts for EN and SV templates
    
    * added missing scripts to build templates to package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,104 +3,173 @@ import inlineCss from 'gulp-inline-css';
 import htmlmin from 'gulp-htmlmin';
 import replace from 'gulp-replace';
 
-import files from './variables';
+import FILES from './variables';
 
 /**
  * Gulp tasks.
- * See https://gulpjs.com/ 
+ * See https://gulpjs.com/
  */
-gulp.task('build:finnish', gulp.series(inlineFI, minifyFI));
-gulp.task('build:swedish', gulp.series(inlineSV, minifySV));
-gulp.task('build:english', gulp.series(inlineEN, minifyEN));
-gulp.task('build:all', gulp.series('build:finnish', 'build:swedish', 'build:english'));
-gulp.task('default', gulp.series('build:finnish', 'build:swedish', 'build:english'));
+gulp.task('build:varaamo:finnish', gulp.series(inlineVaraamoFI, minifyVaraamoFI));
+gulp.task('build:varaamo:swedish', gulp.series(inlineVaraamoSV, minifyVaraamoSV));
+gulp.task('build:varaamo:english', gulp.series(inlineVaraamoEN, minifyVaraamoEN));
+gulp.task('build:varaamo:all', gulp.series('build:varaamo:finnish', 'build:varaamo:swedish', 'build:varaamo:english'));
+gulp.task('build:monitori:finnish', gulp.series(inlineMonitoriFI, minifyMonitoriFI));
+gulp.task('build:monitori:swedish', gulp.series(inlineMonitoriSV, minifyMonitoriSV));
+gulp.task('build:monitori:english', gulp.series(inlineMonitoriEN, minifyMonitoriEN));
+gulp.task('build:monitori:all', gulp.series('build:monitori:finnish', 'build:monitori:swedish', 'build:monitori:english'));
+gulp.task('default', gulp.series('build:varaamo:all', 'build:monitori:all'));
 
 
 /**
  * First replaces placeholder strings(<--feedback/map_url/turku_logo-->) with correct urls for the language,
  * then inlines all css(from css files in /css subfolder) before moving the final product to correct folder/subfolder depending on language.
- * 
- * info on replace/inlineCss:
- * 
+ *
+ * Info on replace/inlineCss:
+ *
  * https://www.npmjs.com/package/gulp-replace
  * https://www.npmjs.com/package/gulp-inline-css
  */
-function inlineFI() {
-    return gulp.src(files.FILE_PATH.fi.inline_path)
-    .pipe(replace(files.LOGO_URLS.src,files.LOGO_URLS.fi))
-    .pipe(replace(files.FEEDBACK_URLS.src,files.FEEDBACK_URLS.fi))
-    .pipe(replace(files.MAP_URLS.src, files.MAP_URLS.fi))
+function inlineVaraamoFI() {
+    return gulp.src(FILES.FILE_PATH.VARAAMO.FI.INLINE_PATH)
+    .pipe(replace(FILES.LOGO_URLS.TURKU.SRC,FILES.LOGO_URLS.TURKU.FI))
+    .pipe(replace(FILES.FEEDBACK_URLS.SRC,FILES.FEEDBACK_URLS.FI))
+    .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.FI))
     .pipe(inlineCss({
-        url: 'file://' + __dirname + '/pages/fi/', // specifies the base path for the stylesheet links
+        url: 'file://' + __dirname + FILES.FILE_PATH.VARAAMO.FI.ROOT, // specifies the base path for the stylesheet links
         applyStyleTags: true,
         applyLinkTags: true,
         removeStyleTags: true,
         removeLinkTags: true
     }))
-    .pipe(gulp.dest(files.FILE_PATH.fi.dest));
+    .pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.FI.DEST));
+}
+
+
+
+/**
+ * @see inlineFI
+ */
+function inlineVaraamoSV() {
+    return gulp.src(FILES.FILE_PATH.VARAAMO.SV.INLINE_PATH)
+    .pipe(replace(FILES.LOGO_URLS.TURKU.SRC, FILES.LOGO_URLS.TURKU.SV))
+    .pipe(replace(FILES.FEEDBACK_URLS.SRC, FILES.FEEDBACK_URLS.SV))
+    .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.SV))
+    .pipe(inlineCss({
+        url: 'file://' + __dirname + FILES.FILE_PATH.VARAAMO.SV.ROOT, // specifies the base path for the stylesheet links
+        applyStyleTags: true,
+        applyLinkTags: true,
+        removeStyleTags: true,
+        removeLinkTags: true
+    })).pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.SV.DEST));
 }
 
 /**
  * @see inlineFI
  */
-function inlineSV() {
-    return gulp.src(files.FILE_PATH.sv.inline_path)
-    .pipe(replace(files.LOGO_URLS.src, files.LOGO_URLS.sv))
-    .pipe(replace(files.FEEDBACK_URLS.src, files.FEEDBACK_URLS.sv))
-    .pipe(replace(files.MAP_URLS.src, files.MAP_URLS.sv))
+function inlineVaraamoEN(){
+    return gulp.src(FILES.FILE_PATH.VARAAMO.EN.INLINE_PATH)
+    .pipe(replace(FILES.LOGO_URLS.TURKU.SRC, FILES.LOGO_URLS.TURKU.EN))
+    .pipe(replace(FILES.FEEDBACK_URLS.SRC, FILES.FEEDBACK_URLS.EN))
+    .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.EN))
     .pipe(inlineCss({
-        url: 'file://' + __dirname + '/pages/sv/', // specifies the base path for the stylesheet links
+        url: 'file://' + __dirname + FILES.FILE_PATH.VARAAMO.EN.ROOT,
         applyStyleTags: true,
         applyLinkTags: true,
         removeStyleTags: true,
         removeLinkTags: true
-    })).pipe(gulp.dest(files.FILE_PATH.sv.dest));
+    })).pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.EN.DEST))
 }
 
-/**
- * @see inlineFI
- */
-function inlineEN(){
-    return gulp.src(files.FILE_PATH.en.inline_path)
-    .pipe(replace(files.LOGO_URLS.src, files.LOGO_URLS.en))
-    .pipe(replace(files.FEEDBACK_URLS.src, files.FEEDBACK_URLS.en))
-    .pipe(replace(files.MAP_URLS.src, files.MAP_URLS.en))
-    .pipe(inlineCss({
-        url: 'file://' + __dirname + '/pages/en/',
-        applyStyleTags: true,
-        applyLinkTags: true,
-        removeStyleTags: true,
-        removeLinkTags: true
-    })).pipe(gulp.dest(files.FILE_PATH.en.dest))
+function inlineMonitoriFI() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.FI.INLINE_PATH)
+        .pipe(replace(FILES.LOGO_URLS.TURKU.SRC,FILES.LOGO_URLS.TURKU.FI))
+        .pipe(replace(FILES.FEEDBACK_URLS.SRC,FILES.FEEDBACK_URLS.FI))
+        .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.FI))
+        .pipe(inlineCss({
+            url: 'file://' + __dirname + FILES.FILE_PATH.MONITORI.FI.ROOT, // specifies the base path for the stylesheet links
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest(FILES.FILE_PATH.MONITORI.FI.DEST));
+}
+
+function inlineMonitoriSV() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.SV.INLINE_PATH)
+        .pipe(replace(FILES.LOGO_URLS.TURKU.SRC,FILES.LOGO_URLS.TURKU.SV))
+        .pipe(replace(FILES.FEEDBACK_URLS.SRC,FILES.FEEDBACK_URLS.SV))
+        .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.SV))
+        .pipe(inlineCss({
+            url: 'file://' + __dirname + FILES.FILE_PATH.MONITORI.SV.ROOT, // specifies the base path for the stylesheet links
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest(FILES.FILE_PATH.MONITORI.SV.DEST));
+}
+
+function inlineMonitoriEN() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.EN.INLINE_PATH)
+        .pipe(replace(FILES.LOGO_URLS.TURKU.SRC,FILES.LOGO_URLS.TURKU.EN))
+        .pipe(replace(FILES.FEEDBACK_URLS.SRC,FILES.FEEDBACK_URLS.EN))
+        .pipe(replace(FILES.MAP_URLS.SERVICE_MAP.SRC, FILES.MAP_URLS.SERVICE_MAP.EN))
+        .pipe(inlineCss({
+            url: 'file://' + __dirname + FILES.FILE_PATH.MONITORI.EN.ROOT, // specifies the base path for the stylesheet links
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest(FILES.FILE_PATH.MONITORI.EN.DEST));
 }
 
 
 /**
  * Removes whitespace for the html files in the build folder.
- * 
+ *
  * info on htmlmin:
  * https://www.npmjs.com/package/gulp-htmlmin
  */
-function minifyFI() {
-    return gulp.src(files.FILE_PATH.fi.minify_path).pipe(htmlmin({
+function minifyVaraamoFI() {
+    return gulp.src(FILES.FILE_PATH.VARAAMO.FI.MINIFY_PATH).pipe(htmlmin({
         collapseWhitespace: true
-    })).pipe(gulp.dest(files.FILE_PATH.fi.dest))
+    })).pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.FI.DEST))
 }
 
 /**
  * @see minifyFI for information
  */
-function minifySV() {
-    return gulp.src(files.FILE_PATH.sv.minify_path).pipe(htmlmin({
+function minifyVaraamoSV() {
+    return gulp.src(FILES.FILE_PATH.VARAAMO.SV.MINIFY_PATH).pipe(htmlmin({
         collapseWhitespace: true
-    })).pipe(gulp.dest(files.FILE_PATH.sv.dest))
+    })).pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.SV.DEST))
 }
 
 /**
  * @see minifyFI
  */
-function minifyEN() {
-    return gulp.src(files.FILE_PATH.en.minify_path).pipe(htmlmin({
+function minifyVaraamoEN() {
+    return gulp.src(FILES.FILE_PATH.VARAAMO.EN.MINIFY_PATH).pipe(htmlmin({
         collapseWhitespace: true
-    })).pipe(gulp.dest(files.FILE_PATH.en.dest))
+    })).pipe(gulp.dest(FILES.FILE_PATH.VARAAMO.EN.DEST))
+}
+
+function minifyMonitoriFI() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.FI.MINIFY_PATH).pipe(htmlmin({
+        collapseWhitespace: true
+    })).pipe(gulp.dest(FILES.FILE_PATH.MONITORI.FI.DEST))
+}
+
+function minifyMonitoriSV() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.SV.MINIFY_PATH).pipe(htmlmin({
+        collapseWhitespace: true
+    })).pipe(gulp.dest(FILES.FILE_PATH.MONITORI.SV.DEST))
+}
+
+function minifyMonitoriEN() {
+    return gulp.src(FILES.FILE_PATH.MONITORI.EN.MINIFY_PATH).pipe(htmlmin({
+        collapseWhitespace: true
+    })).pipe(gulp.dest(FILES.FILE_PATH.MONITORI.EN.DEST))
 }
--- variables.js
+++ variables.js
@@ -1,43 +1,135 @@
+/**
+ * @typedef LanguageSpecificObject
+ * Object containing language specific paths.
+ *
+ * Example shows FI object for varaamo
+ * @example
+ * FI: {
+ *     // path that is used when inlining and replacing
+ *     INLINE_PATH: './pages/varaamo/fi/*.html',
+ *     // path that is used when the html files are minified
+ *     MINIFY_PATH: 'build/varaamo/fi/*.html',
+ *     // path to final product
+ *     DEST: 'build/varaamo/fi/',
+ *     // base path
+ *     ROOT: '/pages/varaamo/fi/',
+ * },
+ */
+
+/**
+ * @typedef ServiceSpecificObject
+ * Contains language specific objects
+ * @see {@link LanguageSpecificObject} example of a language specific object
+ * @example
+ * FI:{},
+ * SV:{},
+ * EN:{}
+ */
+
+/**
+ * @typedef MainObject
+ * Contains service specific objects
+ * @see {@link ServiceSpecificObject} example of service specific object.
+ * @see {@link LanguageSpecificObject} example of language specific object.
+ * @example
+ * VARAAMO:{},
+ * MONITORI:{}
+ */
+
+/**
+ * @typedef PlaceholderStringReplacement
+ * Contains placeholder string SRC that is replaced by a language specific url.
+ *
+ * Example uses FEEDBACK_URLS
+ * @example
+ * SRC: "<--feedback-->",
+ * FI: "https://opaskartta.turku.fi/eFeedback/fi/....",
+ * SV: "https://opaskartta.turku.fi/eFeedback/sv/...",
+ * EN: "https://opaskartta.turku.fi/eFeedback/en/...",
+ */
+
 export default {
+    /** @type {MainObject} **/
     FILE_PATH: {
-        fi: {
-            //path that is used when inlining and replacing
-            inline_path: './pages/fi/*.html',
-            //path that is used when the html files are minified
-            minify_path: 'build/fi/*.html',
-            //path to final product
-            dest: 'build/fi/'
+
+
+        /** @see {@link ServiceSpecificObject} **/
+        VARAAMO: {
+            /** @type {LanguageSpecificObject} **/
+            FI: {
+                INLINE_PATH: './pages/varaamo/fi/*.html',
+                MINIFY_PATH: 'build/varaamo/fi/*.html',
+                DEST: 'build/varaamo/fi/',
+                ROOT: '/pages/varaamo/fi/',
+            },
+            /** @type {LanguageSpecificObject} **/
+            SV: {
+                INLINE_PATH: './pages/varaamo/sv/*.html',
+                MINIFY_PATH: 'build/varaamo/sv/*.html',
+                DEST: 'build/varaamo/sv/',
+                ROOT: '/pages/varaamo/sv/'
+            },
+            /** @type {LanguageSpecificObject} **/
+            EN: {
+                INLINE_PATH: './pages/varaamo/en/*.html',
+                MINIFY_PATH: 'build/varaamo/en/*.html',
+                DEST: 'build/varaamo/en/',
+                ROOT: '/pages/varaamo/en/'
+            },
         },
-        sv: {
-            inline_path: './pages/sv/*.html',
-            minify_path: 'build/sv/*.html',
-            dest: 'build/sv/'
-        },
-        en: {
-            inline_path: './pages/en/*.html',
-            minify_path: 'build/en/*.html',
-            dest: 'build/en/'
+
+        /** @see {@link ServiceSpecificObject} **/
+        MONITORI: {
+            /** @type {LanguageSpecificObject} **/
+            FI: {
+                INLINE_PATH: './pages/monitori/fi/*.html',
+                MINIFY_PATH: 'build/monitori/fi/*.html',
+                DEST: 'build/monitori/fi/',
+                ROOT: '/pages/monitori/fi/'
+            },
+            /** @type {LanguageSpecificObject} **/
+            SV: {
+                INLINE_PATH: './pages/monitori/sv/*.html',
+                MINIFY_PATH: 'build/monitori/sv/*.html',
+                DEST: 'build/monitori/sv/',
+                ROOT: '/pages/monitori/sv/',
+            },
+            /** @type {LanguageSpecificObject} **/
+            EN: {
+                INLINE_PATH: './pages/monitori/en/*.html',
+                MINIFY_PATH: 'build/monitori/en/*.html',
+                DEST: 'build/monitori/en/',
+                ROOT: '/pages/monitori/en/',
+            }
         }
     },
+
+    /** @type {PlaceholderStringReplacement} **/
     FEEDBACK_URLS: {
         //this placeholder string is replaced by the correct one according to language
-        src: "<--feedback-->",
-        fi: "https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut",
-        sv: "https://opaskartta.turku.fi/eFeedback/sv/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut",
-        en: "https://opaskartta.turku.fi/eFeedback/en/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut"
+        SRC: "<--feedback-->",
+        FI: "https://opaskartta.turku.fi/eFeedback/fi/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut",
+        SV: "https://opaskartta.turku.fi/eFeedback/sv/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut",
+        EN: "https://opaskartta.turku.fi/eFeedback/en/Feedback/30-S%C3%A4hk%C3%B6iset%20asiointipalvelut"
     },
+
     MAP_URLS: {
-        //this placeholder string is replaced by the correct one according to language
-        src: "<--map_url-->",
-        fi: "https://palvelukartta.turku.fi/unit/",
-        sv: "https://servicekarta.turku.fi/unit/",
-        en: "https://servicemap.turku.fi/",
+        /** @type {PlaceholderStringReplacement} **/
+        SERVICE_MAP: {
+            //placeholder string that is replaced according to language specific value
+            SRC: "<--map_url-->",
+            FI: "https://palvelukartta.turku.fi/unit/",
+            SV: "https://servicekarta.turku.fi/unit/",
+            EN: "https://servicemap.turku.fi/",
+        }
     },
     LOGO_URLS: {
-        //this placeholder string is replaced by the correct one according to language
-        src: "<--turku_logo-->",
-        fi: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo.png",
-        sv: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo_sv.png",
-        en: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo.png",
+        /** @type {PlaceholderStringReplacement} **/
+        TURKU: {
+            SRC: "<--turku_logo-->",
+            FI: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo.png",
+            SV: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo_sv.png",
+            EN: "http://www.turku.fi/sites/default/files/styles/site_logo/public/sites/all/themes/custom/driveturku/images/sprites/logo.png",
+        }
     },
 };
